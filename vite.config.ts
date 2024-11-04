ab37f304c5f90c1fcb18cc4c2a0cccc02b44b509
Aliaksey Krot <65420310+podpolkownik@users.noreply.github.com>
2024-11-04 10:59:27 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/333/', // Обновляем базовый путь на имя репозитория
+  base: '/333/', // Базовый путь для правильного отображения по адресу https://podpolkownik.github.io/333/
 });
